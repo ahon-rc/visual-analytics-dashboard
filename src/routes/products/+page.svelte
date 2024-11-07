6f84778d96025ad38c38a8e20d74adfac907701c
geb07gek <geb07gek@rhrk.uni-kl.de>
2024-11-07 09:17:39 +0100

    Initial commit

---
+++ src/routes/products/+page.svelte
@@ -0,0 +1,6 @@
+<h3>Product List:</h3>
+<ul>
+	<li>Product 1</li>
+	<li>Product 2</li>
+	<li>Product 3</li>
+</ul>
---
+++ src/routes/products/[productId]/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+    import { page } from '$app/stores';
+    const id = $page.params.productId;
+</script>
+
+<h1>Details about product {id}</h1>
